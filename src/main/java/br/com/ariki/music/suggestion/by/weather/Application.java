9b23d4e9ee85db6de39f476365ce3bd5dcd6b5d5
Daniel Ramon Ariki <dramonar@everis.com>
2019-08-02 22:25:28 -0300

    Commit

--- src/main/java/br/com/ariki/music/suggestion/by/weather/Application.java
+++ src/main/java/br/com/ariki/music/suggestion/by/weather/Application.java
@@ -1,12 +1,9 @@
 package br.com.ariki.music.suggestion.by.weather;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-import br.com.ariki.music.suggestion.by.weather.dataprovider.SearchWeatherByCityDataProvider;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -16,7 +13,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Autowired
-	SearchWeatherByCityDataProvider dataprovider;
 }
